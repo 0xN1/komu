09bd8084993f0aabd622eef0d2469f7e6c85a1b2
Nero One <info@nero1.net>
2025-08-09 10:56:22 +0800

    fix: dynamic date

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,32 @@
+'use client'
+
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const [currentDate, setCurrentDate] = useState("");
+
+  useEffect(() => {
+    // Set initial date
+    updateDate();
+    
+    // Update date every minute
+    const interval = setInterval(updateDate, 60000);
+    
+    // Clean up interval on unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  const updateDate = () => {
+    setCurrentDate(
+      new Date().toLocaleDateString("en-MY", {
+        month: "short",
+        day: "2-digit",
+        year: "2-digit",
+      })
+    );
+  };
+
   return (
     <header className="w-full flex justify-between items-center p-4">
       <Link
@@ -10,11 +36,7 @@ const Header = () => {
         KOMU
       </Link>
       <span className="uppercase">
-        {new Date().toLocaleDateString("en-MY", {
-          month: "short",
-          day: "2-digit",
-          year: "2-digit",
-        })}
+        {currentDate}
       </span>
     </header>
   );

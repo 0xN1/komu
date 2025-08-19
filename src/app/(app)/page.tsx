3395237bb081344d6ea11278f250434fe57e04cd
Nero One <info@nero1.net>
2025-08-19 23:03:29 +0800

    feat: add discord link

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
 import Link from "next/link";
-import {motion as m} from "motion/react"
+import { motion as m } from "motion/react";
 import { fadeIn, staggerAnimation } from "@/lib/anim";
 import { ArrowUpRight } from "lucide-react";
 
@@ -18,16 +18,22 @@ const HomeLinks = [
     label: "sessions",
     url: "/sessions",
   },
+  {
+    label: "community",
+    url: "https://discord.gg/WXkBUPMtT2",
+  },
 ];
-
-
 
 export default function Home() {
   return (
     <>
       <main className="flex flex-col w-full min-h-[60dvh] max-h-[60dvh] gap-[32px] row-start-2 justify-between px-4">
         <div className="flex flex-col gap-4 h-full items-end">
-          <m.div {...fadeIn} transition={{ delay: 0.15, duration: 0.5 }} className="px-6 py-4 outline outline-primary rounded-full text-secondary">
+          <m.div
+            {...fadeIn}
+            transition={{ delay: 0.15, duration: 0.5 }}
+            className="px-6 py-4 outline outline-primary rounded-full text-secondary"
+          >
             A COMMUNITY INITIATIVE
           </m.div>
         </div>
